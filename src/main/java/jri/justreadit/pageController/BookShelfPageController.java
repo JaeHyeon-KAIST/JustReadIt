1a1e905f0ddeedd2b6ac9b52de33fa8152f932ca
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2024-12-15 03:13:45 +0900

    불필요한 변수 제거

--- src/main/java/jri/justreadit/pageController/BookShelfPageController.java
+++ src/main/java/jri/justreadit/pageController/BookShelfPageController.java
@@ -1,18 +1,13 @@
 package jri.justreadit.pageController;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import jri.justreadit.JRIApp;
-import jri.justreadit.scenario.BookDetailScenario;
 import jri.justreadit.scenario.BookShelfScenario;
 import x.XPageController;
 
 public class BookShelfPageController extends XPageController {
   public static final String PAGE_CONTROLLER_NAME = "BookShelfPageController";
   public static final String FXML_NAME = "BookShelfPage";
-
-  @FXML
-  private Button goToHomePageButton;
 
   public BookShelfPageController(JRIApp app, String fxmlBasePath) {
     super(PAGE_CONTROLLER_NAME, fxmlBasePath, FXML_NAME, app);
